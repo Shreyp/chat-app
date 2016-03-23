3c658fe1ed321633580abe409cb172f1b986d37a
unknown <shreyansap@gmail.com>
2016-03-23 11:10:02 -0400

    added associate.me files

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var express = require('express');
+var logger = require('morgan');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.get('/chat', function(req,res){
+  res.sendFile(process.cwd() + '/views/chat.html');
+});
+
+app.get('/event', function(req,res){
+  res.sendFile(process.cwd() + '/views/event.html');
+});
+
+app.use('/images', express.static('assets/images'));
+app.use('/css', express.static('assets/css'));
+app.use('/js', express.static('assets/js'));
+app.use(logger('dev'));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
